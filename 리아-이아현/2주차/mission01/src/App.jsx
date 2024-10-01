e0b203fab363cfe432cee9da1b7a21fd6921e5f6
[SW5] 이아현 <laah0602@gmail.com>
2024-10-01 15:48:20 +0900

    feat: toDoList 추가,수정,삭제 기능 구현

---
+++ 리아-이아현/2주차/mission01/src/App.jsx
@@ -0,0 +1,93 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState([{ id: 1, task: "투두 만들어보기" }]);
+  const [text, setText] = useState("");
+  const [editingId, setEditingId] = useState("");
+  const [editText, setEditText] = useState("");
+
+  // 렌더링 방지
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  // 1. 추가하기
+  const addTodo = () => {
+    if (text.trim().length === 0) {
+      alert("할 일을 입력하세요");
+    }
+    setTodos((prev) => [
+      ...prev,
+      { id: Math.floor(Math.random() * 100) + 2, task: text },
+    ]);
+    setText("");
+  };
+
+  // 2. 삭제하기
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  // 3. 수정하기
+  const updateTodo = (id, text) => {
+    setTodos((prev) =>
+      prev.map((item) => (item.id === id ? { ...item, task: text } : item))
+    );
+    setEditingId("");
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        <button onClick={() => addTodo()} type="submit">
+          할 일 등록
+        </button>
+      </form>
+      <div>
+        {todos.map((todo, _) => (
+          // eslint-disable-next-line react/jsx-key
+          <div key={todo.id} style={{ display: "flex", gap: "20px" }}>
+            {/*수정이 아닐 때 */}
+            {editingId !== todo.id && (
+              <div key={todo.id} style={{ display: "flex", gap: "5px" }}>
+                <p>{todo.id}.</p>
+                <p>{todo.task}</p>
+              </div>
+            )}
+            {/*수정 중일 때 */}
+            {editingId === todo.id && (
+              <div key={todo.id} style={{ display: "flex", gap: "5px" }}>
+                <p>{todo.id}.</p>
+                <input
+                  defaultValue={todo.task}
+                  onChange={(e) => setEditText(e.target.value)}
+                />
+              </div>
+            )}
+
+            <button onClick={() => deleteTodo(todo.id)}>삭제하기</button>
+            {/* editingId !== todo.id 수정이 아닌 상태 */}
+            {editingId === todo.id ? (
+              <button onClick={() => updateTodo(editingId, editText)}>
+                수정 완료
+              </button>
+            ) : (
+              <button onClick={() => setEditingId(todo.id)}>수정 진행</button>
+            )}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
