a1069abb6869ee4312804ad72e4e8c8ff0ec9fa2
[SW5] 이아현 <laah0602@gmail.com>
2024-10-02 01:22:49 +0900

    style: 마우스 호버 상태에 따른 스타일링 적용

--- 리아-이아현/2주차/mission02/src/components/Movie.jsx
+++ 리아-이아현/2주차/mission02/src/components/Movie.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
+
 const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500/";
 
 export default function Movie({ poster_path }) {
+  const [hovered, setHovered] = useState(false);
+
   const movieStyle = {
     width: 180,
     borderRadius: 10,
     overflow: "hidden",
     textAlign: "center",
+    filter: hovered ? "brightness(50%)" : "brightness(100%)",
   };
 
   return (
-    <div style={movieStyle}>
+    <div
+      style={movieStyle}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
       <img
         src={IMG_BASE_URL + poster_path}
         alt="포스터 사진"
