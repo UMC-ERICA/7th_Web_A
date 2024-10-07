963f41218207f1b70dd1007e2b9cfabc4f701993
gyuhochoime <cjg1999@naver.com>
2024-10-08 00:03:57 +0900

    refactor:<form>과 css border-bottom 추가

--- 호담-최규호/1주차/src/App.js
+++ 호담-최규호/1주차/src/App.js
@@ -10,15 +10,16 @@ const App = () => {
 
   return (
     <div className="container">
-      <h1 className="centered-text">UMC Study Plan</h1>
-      <hr className="divider" />
-      <input 
-        type="text" 
-        className="input-field" 
-        placeholder="스터디 계획을 작성해보세요!" 
-        value={inputValue} 
-        onChange={handleChange} 
-      />
+      <h1 className="centered-text with-border">UMC Study Plan</h1>
+        <form>
+          <input 
+          type="text" 
+          className="input-field" 
+          placeholder="스터디 계획을 작성해보세요!" 
+          value={inputValue} 
+          onChange={handleChange} 
+          />
+        </form>
       <div className="task-container">
         <h2 className="task">해야 할 일</h2>
         <h2 className="achievement">해낸 일</h2>
