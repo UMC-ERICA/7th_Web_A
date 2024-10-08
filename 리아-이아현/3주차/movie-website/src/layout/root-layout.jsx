51803560de1ecae6b0151d40e48b9a8ba83b42f5
[SW5] 이아현 <laah0602@gmail.com>
2024-10-08 16:51:00 +0900

    feat: 레이아웃 쪼개기

---
+++ 리아-이아현/3주차/movie-website/src/layout/root-layout.jsx
@@ -0,0 +1,25 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/NavBar.jsx";
+import Sidebar from "../components/SideBar.jsx";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+`;
+
+const RootLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <MainContainer>
+        <Sidebar />
+        <Outlet />
+      </MainContainer>
+      <Outlet />
+    </>
+  );
+};
+
+export default RootLayout;
