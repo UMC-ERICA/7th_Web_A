aa8346dcebc78cfe7d85de60225efe77c38aa432
[SW5] 이아현 <laah0602@gmail.com>
2024-10-08 21:06:27 +0900

    style: sidebar 스타일링

--- 리아-이아현/3주차/movie-website/src/components/SideBar.jsx
+++ 리아-이아현/3주차/movie-website/src/components/SideBar.jsx
@@ -1,8 +1,55 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { FaSearch } from "react-icons/fa";
+import { BiSolidCameraMovie } from "react-icons/bi";
+
+const SidebarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px 20px;
+  background-color: #333;
+  color: white;
+  width: 150px;
+  height: 100vh;
+`;
+
+const SideButtons = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+const Button = styled(Link)`
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  font-weight: bold;
+  font-size: 15px;
+  background-color: ${(props) => props.color};
+  padding: 15px;
+  cursor: pointer;
+  color: white;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
 const Sidebar = () => {
   return (
-    <div>
-      <h2>Sidebar</h2>
-    </div>
+    <SidebarContainer>
+      <SideButtons>
+        <Button color={"#333"}>
+          <FaSearch />
+          찾기
+        </Button>
+        <Button color={"#333"}>
+          <BiSolidCameraMovie />
+          영화
+        </Button>
+      </SideButtons>
+    </SidebarContainer>
   );
 };
 
